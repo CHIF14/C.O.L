1d6472b15a71794a34d7366cebd52ebadb0991b7
lukas <resch.lukas10@gmail.com>
2015-04-16 20:49:56 -0700

    Output: Hallo zurueck

--- src/chif14/MAIN.java
+++ src/chif14/MAIN.java
@@ -7,7 +7,8 @@ package chif14;
 public class MAIN {
     
     public static void main(String[] args) {
-      System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("Hallo zurück");
     }
     
 }
