8b76aac4abdab32f484d2d1413fb9d38e0cea288
lukas <resch.lukas10@gmail.com>
2015-04-19 22:17:02 -0700

    Anfang

---
+++ src/chif14/Draw.java
@@ -0,0 +1,25 @@
+package chif14;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author lukas
+ */
+public class Draw extends JPanel{
+    
+    public float playerX = 50, playerY = 50;
+    
+    protected void paintComponent(Graphics g)
+    {
+        g.setColor(Color.orange);
+        g.fillRect((int)playerX, (int)playerY, 25, 25);
+    }
+    public void playerXp() {playerX++;}
+    public void playerXm() {playerX--;}
+    public void playerYp() {playerY++;}
+    public void playerYm() {playerY--;}
+    
+}
--- src/chif14/MAIN.java
+++ src/chif14/MAIN.java
@@ -1,13 +1,83 @@
 package chif14;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+
 /**
  *
  * @author lukas
  */
-public class MAIN {
+public class MAIN implements Runnable, KeyListener{
     
-    public static void main(String[] args) {
-	
+    Draw dw;
+    JFrame frame;
+    boolean playerxPlus = false, playerxMinus = false, playeryPlus = false, playeryMinus = false;
+    
+    public MAIN()
+    {
+        newFrame();
     }
     
+    public void newFrame()
+    {
+        frame = new JFrame("C.O.L");
+        frame.setSize(800,450);
+        frame.addKeyListener(this);
+        
+        dw = new Draw();
+        frame.add(dw);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+        new Thread(this).start();
+    }
+    
+    public void run()
+    {
+        while(true)
+        {
+            dw.repaint();
+            
+            if(playeryMinus) dw.playerYm();
+            if(playeryPlus) dw.playerYp();
+            if(playerxMinus) dw.playerXm();
+            if(playerxPlus) dw.playerXp();
+            
+            try {
+                Thread.sleep(4);
+            } catch (Exception ex) {
+                Logger.getLogger(MAIN.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {new MAIN();}
+
+    
+    // <editor-fold defaultstate="collapsed" desc="KeyListener">
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_W) playeryMinus = true;
+        if(e.getKeyCode() == KeyEvent.VK_S) playeryPlus = true;
+        if(e.getKeyCode() == KeyEvent.VK_A) playerxMinus = true;
+        if(e.getKeyCode() == KeyEvent.VK_D) playerxPlus = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_W) playeryMinus = false;
+        if(e.getKeyCode() == KeyEvent.VK_S) playeryPlus = false;
+        if(e.getKeyCode() == KeyEvent.VK_A) playerxMinus = false;
+        if(e.getKeyCode() == KeyEvent.VK_D) playerxPlus = false;
+    }
+    // </editor-fold>
 }
