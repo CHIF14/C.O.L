5262c0dc8f30c8872090ed5984425bd4a837d692
lukas <resch.lukas10@gmail.com>
2015-04-20 19:55:06 -0700

    Fullscreen

--- src/chif14/Draw.java
+++ src/chif14/Draw.java
@@ -2,6 +2,7 @@ package chif14;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 /**
@@ -13,24 +14,25 @@ public class Draw extends JPanel{
     public float playerX = 50, playerY = 50;
     public int playerSize = 25;
     public float velocity = 1;
+    public float scaleX = 1.0f, scaleY = 1.0f;
     
     protected void paintComponent(Graphics g)
     {
 	super.paintComponent(g);
-        g.setColor(Color.orange);
+        Graphics2D gr = (Graphics2D) g;
+        gr.scale(scaleX, scaleY);
 //        g.fillRect((int)playerX, (int)playerY, playerSize, playerSize);
 	g.drawImage(Imports.image.get(Imports.player), (int) playerX, (int) playerY, playerSize, playerSize, this);
     }
-    
     // <editor-fold defaultstate="collapsed" desc="Player movement">
     public void playerXp() {
-      if((playerX + playerSize) < getWidth()) playerX += velocity;
+      if((playerX + playerSize) < 800) playerX += velocity;
     }
     public void playerXm() {
       if(playerX > 0) playerX -= velocity;
     }
     public void playerYp() {
-      if((playerY + playerSize) < getHeight()) playerY += velocity;
+      if((playerY + playerSize) < 450) playerY += velocity;
     }
     public void playerYm() {
       if(playerY > 0) playerY -= velocity;
--- src/chif14/MAIN.java
+++ src/chif14/MAIN.java
@@ -1,5 +1,8 @@
 package chif14;
 
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -30,26 +33,22 @@ public class MAIN implements Runnable, KeyListener{
     {
         frame = new JFrame("C.O.L");
         frame.setSize(800,450);
-	
-	if(fullscreen) {
-	  frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
-	}
-	
-	// Added for fullscreen purposes - press ALT+F4 until better fix
 	frame.setUndecorated(true);
         frame.addKeyListener(this);
         
-        if(firstStart) {
-	  dw = new Draw();
-	}
+        dw = new Draw();
         frame.add(dw);
+        
+        dw.scaleX = frame.getWidth() / 800;
+        dw.scaleY = frame.getHeight() / 450;
 	
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setVisible(true);
         
-        if(firstStart) new Thread(this).start();
-	firstStart = false;
+        new Thread(this).start();
+	
     }
     
     public void run()
@@ -73,26 +72,48 @@ public class MAIN implements Runnable, KeyListener{
     
     public static void main(String[] args) {new MAIN();}
     
-    private void switchFullscreen() {      
-      if(fullscreen) {
-	fullscreen = false;
-	frame.dispose();
-	newFrame();
-	dw.playerSize = 25;
-	dw.velocity = 1;
-	dw.playerX *= .416f;
-	dw.playerY *= .416f;
-      } else {
-	fullscreen = true;
-	frame.dispose();
-	newFrame();
-	float numberX = (1f/800f) * (float) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-	float numberY = (1f/450f) * (float) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-	dw.playerSize *= numberX;
-	dw.velocity = numberX;
-	dw.playerX *= numberX;
-	dw.playerY *= numberY;
-      }
+    private void switchFullscreen() {   
+        // <editor-fold defaultstate="collapsed" desc="unötig!">
+//      if(fullscreen) {
+//	fullscreen = false;
+//	frame.dispose();
+//	newFrame();
+//	dw.playerSize = 25;
+//	dw.velocity = 1;
+//	dw.playerX *= .416f;
+//	dw.playerY *= .416f;
+//      } else {
+//	fullscreen = true;
+//	frame.dispose();
+//	newFrame();
+//	float numberX = (1f/800f) * (float) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+//	float numberY = (1f/450f) * (float) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+//	dw.playerSize *= numberX;
+//	dw.velocity = numberX;
+//	dw.playerX *= numberX;
+//	dw.playerY *= numberY;
+//      }
+        // </editor-fold>
+        
+        GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice divice = environment.getDefaultScreenDevice();
+        if(!fullscreen)
+        {
+            fullscreen = true;
+            Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        
+            dw.scaleX = (float) (d.width) / 800;
+            dw.scaleY = (float) (d.height) / 450;
+        
+            divice.setFullScreenWindow(frame);
+        }else{
+            divice.setFullScreenWindow(null);
+            
+            dw.scaleX = 1.0f;
+            dw.scaleY = 1.0f;
+            
+            fullscreen = false;
+        }
     }
     
     // <editor-fold defaultstate="collapsed" desc="KeyListener">
